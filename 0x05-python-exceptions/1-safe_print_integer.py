30a63ba7897f4650c59036c5cc5b5fb4d67c8f0e
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-04-12 05:01:59 -0700

    update {1-safe_print_integer new edit}

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if "{:d}".format(value):
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
+        print("{:d}".format(value))
+        return True
     except ValueError:
-        pass
+        return False
