e7a67ebf4bafa0db954ad13b3f525a00531bfc4f
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-04-13 08:08:08 -0700

    update {code edit}

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 class Square:
     """ A simple class to describe a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 class Square:
     """ A simple class to describe a square """
+
+
     def __init__(self, size) -> None:
-        """
-        args:
-            size (str): Size of a square
+        """Initializing square properties
+
+        Args: size (int): Size of a square
         """
         self.__size = size
