7aba681540b87c316e9b170f96c4ead3850e3fe4
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-04-13 08:31:24 -0700

    update {code edit}

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,25 @@
+#!/usr/bin/python3
+
+""" Module defines a square """
+
+
 class Square:
     """ A simple class to define a square"""
     def __init__(self, size=0):
         """
         Conditional Loop below to check if value enters is an
         integer and if size > 0
-        
+
         Args:
             size(int): size of square,default value is 0
+
+        Raises:
+            TypeError: if size is not and integer
+            ValueError: if size less than or equals to zero
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
         else:
             if size < 0:
                 raise ValueError("size must be >= 0")
-            
             self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+""" modules defines a square """
+
+
 class Square:
+
     """ A class to describe a square
-    
+
     Attributes:
         __size (int): size of square
     """
@@ -9,10 +14,10 @@ class Square:
         """
         Conditional Loop below to check if value enters is an
         integer and if size < 0
-        
+
         Args:
             size(int): size of square,default value is 0
-        
+
         Raises:
             TypeError: if size is not an integer
             ValueError: if size is less than 0 """
@@ -22,12 +27,11 @@ class Square:
             if size < 0:
                 raise ValueError("Size must be >= 0")
             self.__size = size
-            
+
     def area(self):
         """Calculate the area of the square
-        
+
         Returns:
             int: Area of the square
         """
         return (self.__size)**2
-    
