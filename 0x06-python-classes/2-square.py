321d5b1ac86103dafdd49cbd9f2b2b276b9fb050
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-05-12 10:02:02 -0700

    2-square edit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -20,7 +20,7 @@ class Square:
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
