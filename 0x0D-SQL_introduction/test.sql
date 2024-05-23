0369edbc95466204243ea520b63af95f0fd4d18b
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-05-22 17:26:54 -0700

    avg_temp mysql query script

--- 0x0D-SQL_introduction/test.sql
+++
@@ -1,5 +0,0 @@
--- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table 
-VALUES (5, 'ARIAL', 18), (6, 'Arial', 12), (30, 'NULL', 14), (23, NULL , 8);
