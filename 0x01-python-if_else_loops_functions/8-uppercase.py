cca7db393f8f5ef7ba442107859a5301e4292247
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-16 10:18:55 -0700

    8-uppercase.py requirment fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
+    store = ""
     for i in str:
         if ord(i) >= 97 and ord(i) <= 123:
             placement_in_alphabet_chain = ord(i) - ord('a')
             upper_value = ord('A') + placement_in_alphabet_chain
-            print("{}".format(chr(upper_value)), end="")
+            store += chr(upper_value)
         else:
-            print("{}".format(i), end="")
-    print("\n")
+            store += i
+    print("{}".format(store), end="")
+    print("")
