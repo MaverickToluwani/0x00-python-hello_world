0e6f53ef297c77518308848138f8a2fdacd3b497
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-16 10:23:27 -0700

    9-print_last_digit.py code fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,8 @@
 def print_last_digit(number):
     if number >= 0:
         last_digit = number % 10
-        print(last_digit)
     elif number < 0:
         last_digit = (-1*number) % 10
-        print(last_digit)
+
+    print(last_digit, end="")
     return last_digit
