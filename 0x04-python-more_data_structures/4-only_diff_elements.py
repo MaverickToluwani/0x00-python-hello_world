382e6d04a4e3209acd5f3fec0bec6e563dffa929
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-17 00:58:03 -0700

    4-only_diff_elements.py update

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     diff_ele = []
+    if set_1 == []:
+        return set_2
+    if set_2 == []:
+        return set_1
     for i in set_1:
         for j in set_2:
             if j not in set_1 and j not in diff_ele:
