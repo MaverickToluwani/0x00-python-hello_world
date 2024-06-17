7016cce1ee34f535d6826f58fef8e43c52216dfc
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-17 14:02:38 -0700

    12-roman_to_int.py update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def roman_to_int(roman_string) -> int:
     tbl = {
             'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5,
@@ -9,7 +8,10 @@ def roman_to_int(roman_string) -> int:
             'DC': 600, 'DCC': 700, 'DCCC': 800, 'CM': 900,
             'M': 1000, 'MM': 2000, 'MMM': 3000
             }
+
     Rn = roman_string
+    if type(Rn) != str or Rn is None:
+        return 0
     if roman_string in tbl:
         return tbl[roman_string]
     elif Rn not in tbl:
@@ -17,34 +19,39 @@ def roman_to_int(roman_string) -> int:
             if len(Rn) == 1:
                 return tbl[Rn[0]]
             elif len(Rn) == 2:
-                return tbl[Rn[0]] + tbl[Rn[1]]
+                return tbl[Rn[0:]]
         elif len(Rn) >= 3 and len(Rn) < 6:
-            c3 = tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]]
+            c3 = tbl[Rn[0]] + tbl[Rn[1]]
             if len(Rn) == 3:
                 return c3
             elif len(Rn) == 4:
-                return c3 + tbl[Rn[3]]
+                return tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2:]]
             elif len(roman_string) == 5:
-                return c3 + tbl[Rn[3]] + tbl[Rn[4]]
+                return tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]] +\
+                       tbl[Rn[3:]]
         elif len(Rn) >= 6 and len(Rn) < 9:
-            c3 = tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]]
-            c4_6 = tbl[Rn[3]] + tbl[Rn[4]] + tbl[Rn[5]]
             if len(Rn) == 6:
-                return c3 + c4_6
+                return tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]] +\
+                       tbl[Rn[3]] + tbl[Rn[4:]]
             elif len(Rn) == 7:
-                return c3 + c4_6 + tbl[Rn[6]]
+                return tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]] +\
+                       tbl[Rn[3]] + tbl[Rn[4]] + tbl[Rn[5:]]
             elif len(roman_string) == 8:
-                return c3 + c4_6 + tbl[Rn[6]] + tbl[Rn[7]]
+                return tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]] +\
+                       tbl[Rn[3]] + tbl[Rn[4]] + tbl[Rn[5]] + tbl[Rn[6:]]
         elif len(Rn) >= 9 and len(Rn) < 12:
-            c3 = tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]]
-            c4_6 = tbl[Rn[3]] + tbl[Rn[4]] + tbl[Rn[5]]
-            c7_9 = tbl[Rn[6]] + tbl[Rn[7]] + tbl[Rn[8]]
-            c10_11 = tbl[Rn[9]] + tbl[Rn[10]]
             if len(Rn) == 9:
-                return c3 + c4_6 + c7_9
+                return tbl[Rn[0]] + tbl[Rn[1]] + tbl[Rn[2]] +\
+                       tbl[Rn[3]] + tbl[Rn[4]] + tbl[Rn[5]] +\
+                       tbl[Rn[6]] + tbl[Rn[7:]]
             elif len(Rn) == 10:
-                return c3 + c4_6 + c7_9 + tbl[Rn[9]]
+                return tbl[Rn[0]] + tbl[Rn[1]] +\
+                       tbl[Rn[2]] + tbl[Rn[3]] + tbl[Rn[4]] +\
+                       tbl[Rn[5]] + tbl[Rn[6]] + tbl[Rn[7]] + tbl[Rn[8:]]
             elif len(Rn) == 11:
-                return c3 + c4_6 + c7_9 + c10_11
+                return tbl[Rn[0]] + tbl[Rn[1]] +\
+                       tbl[Rn[2]] + tbl[Rn[3]] + tbl[Rn[4]] +\
+                       tbl[Rn[5]] + tbl[Rn[6]] + tbl[Rn[7]] +\
+                       tbl[Rn[8]] + tbl[Rn[9:]]
     else:
         return
