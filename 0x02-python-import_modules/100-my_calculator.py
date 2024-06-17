a2bce26190f12768feea3de84ce696e84682acaa
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-16 21:57:56 -0700

    100-my_calculator.py requirmet fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,32 +3,30 @@ import sys
 from calculator_1 import add, sub, div, mul
 
 if __name__ == "__main__":
-    msg = "Unknown operator. Available operators: +, -, * and /"
-    if len(sys.argv) < 4:
-        sys.stderr.write(msg)
-
     def calculator():
+        usg = "Usage: ./100-my_calculator.py <a> <operator> <b>\n"
+        msg = "Unknown operator. Available operators: +, -, * and /\n"
         if len(sys.argv) < 4:
-            sys.stderr.write(msg)
-            return 1
+            sys.stderr.write(usg)
+            sys.exit(1)
 
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         sign = sys.argv[2]
         if sign == "+":
             print("{} {} {} = {} ".format(a, sign, b, add(a, b)))
-            return 0
+            sys.exit(0)
         elif sign == "-":
             print("{} {} {} = {} ".format(a, sign, b, sub(a, b)))
-            return 0
+            sys.exit(0)
         elif sign == "*":
             print("{} {} {} = {} ".format(a, sign, b, mul(a, b)))
-            return 0
+            sys.exit(0)
         elif sign == "/":
             print("{} {} {} = {} ".format(a, sign, b, div(a, b)))
-            return 0
+            sys.exit(0)
         else:
             sys.stderr.write(msg)
-            return 1
+            sys.exit(1)
 
 print(calculator())
