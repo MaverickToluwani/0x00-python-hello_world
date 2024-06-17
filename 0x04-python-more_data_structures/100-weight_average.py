9e6272ab33f6644c2bbaf2245612e26887194df2
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-17 01:03:28 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if len(my_list) == 0:
-        return
+        return 0
     sum_SW = 0
     total_weight = 0
     for row in range(len(my_list)):
