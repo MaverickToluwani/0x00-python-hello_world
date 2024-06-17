637ef64fc5bc9a9c41fadda005a816758132aec6
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-17 00:49:58 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    sent_len = len(sentence)
-    firstChar = sentence[0]
-
     if sentence == "" or sentence is None:
         return 0, None
-    else:
-        return sent_len, firstChar
+
+    sent_len = len(sentence)
+    firstChar = sentence[0]
+    return sent_len, firstChar
