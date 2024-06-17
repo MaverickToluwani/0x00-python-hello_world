4728b1bc65aa5aa791e2ca34a8acdaf3e6e4b52c
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-17 00:21:47 -0700

    4-new_in_list.py update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,10 +3,8 @@ def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     list_len = len(my_list)
 
-    if idx < 0:
-        return
-    elif idx >= list_len:
-        return
+    if idx < 0 or idx >= list_len:
+        return my_list
 
     list_copy[idx] = element
     return list_copy
