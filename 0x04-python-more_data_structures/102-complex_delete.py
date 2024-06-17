39e8b355b36abc92c594afb1f6b42169358a9300
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-17 14:58:24 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,6 +5,6 @@ def complex_delete(a_dictionary, value):
     count = 0
     for val in val_list:
         if val == value:
-            del(a_dictionary[key_list[count]])
+            del (a_dictionary[key_list[count]])
         count += 1
     return a_dictionary
