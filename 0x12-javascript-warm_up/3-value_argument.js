ca4fe7bad65334ffebcc51522ee16f5e7714805b
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-19 19:39:26 -0700

    3-value_argument.s update  '

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-let count = 0;
-let i;
-for (i in args) {
-  count++;
-}
-if (count <= 2) {
+if (!args[2]) {
   console.log('No argument');
 } else {
   console.log(args[2]);
-  }
+}
