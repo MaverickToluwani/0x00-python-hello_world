3004946d8cdf8aeedfd1f5e46feb43c880556fc2
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-25 16:14:56 -0700

    more script

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,7 +2,8 @@
 """ A script that takes in an argument and
     displays all values in the states table
     of hbtn_0e_0_usa where name matches the
-    argument
+    argument. parameterized queries are used
+    to avoid attacks by sql injection
 """
 
 import MySQLdb
@@ -21,8 +22,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(
             "SELECT * FROM states\
-                    WHERE name LIKE BINARY '{}'\
-                    ORDER BY states.id ASC".format(argv[4])
+                    WHERE name LIKE BINARY %s\
+                    ORDER BY states.id ASC", (argv[4], )
                     )
     rows = cur.fetchall()
     for row in rows:
