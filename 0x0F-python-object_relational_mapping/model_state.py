3e343660bb1bf9e2d3ffa0942ddef5da09bf7ebb
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-26 09:38:31 -0700

    sqlalchemy orm

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A script that creates a class
+    definition of a State, which will be translated
+    to a MySQL database table
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ A class definition of a State """
+    __tablename__ = 'states'
+
+    id = Column(
+            Integer,
+            primary_key=True,
+            autoincrement=True,
+            unique=True,
+            nullable=False
+            )
+    name = Column(String(128), nullable=False)
