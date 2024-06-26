d82bee8030c127148c4f6e67c11a42550cd8b04e
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-25 17:49:54 -0700

    5-filter_cities.py addition

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
     cur.execute(
             "SELECT cities.name FROM cities INNER JOIN states\
                     ON cities.state_id = states.id\
-                    WHERE states.name = %s", (argv[4], )
+                    WHERE states.name = %s\
+                    ORDER BY cities.id ASC", (argv[4], )
                     )
     rows = cur.fetchall()
     num_cities = len(rows)
