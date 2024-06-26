871120c0f5238b7deb241f0f00c7157fbfaf90f5
Toluwani Lekan-Afinni <micklegend089@gmail.com>
2024-06-26 11:28:29 -0700

    sqlalchemy filter script

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" A script that prints the first
+    State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]
+            )
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    res = session.query(State.id, State.name).filter(State.id == 1).first()
+    print(f"{res[0]}: {res[1]}")
+
+    session.close()
